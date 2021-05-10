54332143c10af4005f10d70454a4013cea3d0341
Crowdbotics <team@crowdbotics.com>
2021-05-10 18:11:18 +0000

    Initial commit.

---
+++ backend/lively_tree_26508/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lively_tree_26508 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lively_tree_26508.settings')
+
+application = get_wsgi_application()
